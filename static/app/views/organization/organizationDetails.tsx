2820f8287305e2f8f08ac2a92b26b8bddc5dcfc1
Geetendra1 <sengargeetendra123@gmail.com>
2023-01-09 20:30:06 +0530

    use of api.tsx

--- static/app/views/organization/organizationDetails.tsx
+++ static/app/views/organization/organizationDetails.tsx
@@ -2,17 +2,28 @@ import {Fragment, useEffect, useState} from 'react';
 import {RouteComponentProps} from 'react-router';
 import OrganizationsStore from 'cliff/stores/organizationsStore';
 import {Organization} from 'cliff/types';
-
+import {Client} from 'cliff/api';
 type RouteParams = {
   orgId: string;
 };
 
-type Props = RouteComponentProps<RouteParams, {}>;
+type Props = RouteComponentProps<RouteParams, {} & {
+  api: Client;
+}>;
 
 function OrganizationDetails(props: Props) {
   const [orgs, setOrgs] = useState<Organization[]>([]);
 
   useEffect(() => {
+    const api = new Client()
+
+    // replace todos with with your backend api endpoint 
+    // after using api.tsx your api call will looks like 'http://localhost:6001/api/0/todos' where 'http://localhost:6001/api/0/ is written in api.tsx, you can configure it accordingly 
+    const res =  api.requestPromise('/todos', {
+      method: 'GET',
+    });
+    console.log('res',res);
+
     setOrgs(OrganizationsStore.getAll());
   }, [orgs]);
 
